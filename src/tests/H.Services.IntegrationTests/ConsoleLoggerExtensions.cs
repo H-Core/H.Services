86f4495eebe9a54194d1a871914a54ab6d60d402
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-16 22:02:47 +1000

    Added HookService tests.

---
+++ src/tests/H.Services.IntegrationTests/ConsoleLoggerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core.Utilities;
+using H.Services.Core;
+
+namespace H.Services.IntegrationTests
+{
+    public static class ConsoleLoggerExtensions
+    {
+        public static void EnableLogging(
+            this IServiceBase service, 
+            ExceptionsBag? exceptions = null, 
+            CancellationTokenSource? source = null)
+        {
+            service.ExceptionOccurred += (_, exception) =>
+            {
+                Console.WriteLine($"{nameof(service.ExceptionOccurred)}: {exception}");
+                exceptions?.OnOccurred(exception);
+                source?.Cancel();
+            };
+            if (service is ICommandProducer commandProducer)
+            {
+                commandProducer.CommandReceived += (_, value) =>
+                {
+                    Console.WriteLine($"{nameof(commandProducer.CommandReceived)}: {value}");
+                };
+                commandProducer.AsyncCommandReceived += (_, value, _) =>
+                {
+                    Console.WriteLine($"{nameof(commandProducer.AsyncCommandReceived)}: {value}");
+
+                    return Task.CompletedTask;
+                };
+            }
+            if (service is HookService hookService)
+            {
+                hookService.CombinationCaught += (_, value) =>
+                {
+                    Console.WriteLine($"{nameof(hookService.CombinationCaught)}: {value}");
+                };
+            }
+        }
+    }
+}
---
+++ src/tests/H.Services.IntegrationTests/HookServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace H.Services.IntegrationTests
+{
+    [TestClass]
+    public class HookServiceTests
+    {
+        [TestMethod]
+        public async Task BaseTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var cancellationToken = cancellationTokenSource.Token;
+
+            await using var service = new HookService();
+            var exceptions = new ExceptionsBag();
+            service.EnableLogging(exceptions, cancellationTokenSource);
+
+            await service.InitializeAsync(cancellationToken);
+            
+            await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+            
+            exceptions.EnsureNoExceptions();
+        }
+    }
+}
