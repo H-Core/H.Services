0a2c3d62b87f23464a98a11eda045892c5e8f9b2
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 14:02:39 +1000

    Added IntegrationRunner.

---
+++ src/libs/H.Services.Core/IntegrationRunner.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using H.Core;
+using H.Core.Runners;
+
+namespace H.Services.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class IntegrationRunner : Runner
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IntegrationRunner()
+        {
+            Add(AsyncAction.WithSingleArgument("say", SayAsync, "Arguments: text"));
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task SayAsync(string text, CancellationToken cancellationToken = default)
+        {
+            var values = await RunAsync(new Command("synthesize", text, "raw"), cancellationToken)
+                .ConfigureAwait(false);
+            var value = values.First();
+
+            await RunAsync(new Command("play", new Value("raw")
+            {
+                Data = value.Data,
+            }), cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
