77305478eb4152d32fc04d111fc761b41eb4def5
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-14 17:24:00 +1000

    feat: Changed PreviewWindow visual.

--- src/apps/H.Services.Apps.WPF/Views/PreviewWindow.xaml.cs
+++ src/apps/H.Services.Apps.WPF/Views/PreviewWindow.xaml.cs
@@ -16,16 +16,29 @@ namespace H.Services.Apps.Views
         {
             InitializeComponent();
 
-            Width = SystemParameters.VirtualScreenWidth;
-            Height = SystemParameters.VirtualScreenHeight;
-            Left = SystemParameters.VirtualScreenLeft;
-            Top = SystemParameters.VirtualScreenTop;
+            //var primaryScreen = Screen.PrimaryScreen;
+            //var test = SystemParameters.PrimaryScreenWidth;
+            //var test2 = SystemParameters.VirtualScreenHeight;
+            //Width = primaryScreen.WorkingArea.Width;
+            //Height = primaryScreen.WorkingArea.Height;
+            //Left = primaryScreen.WorkingArea.Left;
+            //Top = primaryScreen.WorkingArea.Top;
 
             this.WhenActivated(disposable =>
             {
                 this.OneWayBind(ViewModel,
                         static viewModel => viewModel.Text,
                         static view => view.TextBlock.Text)
+                    .DisposeWith(disposable);
+
+                var showHideCommand = ReactiveCommand.Create<bool>(value =>
+                {
+                    TextBlock.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+                });
+                this.WhenAnyValue(
+                        static x => x.TextBlock.Text,
+                        static value => !string.IsNullOrWhiteSpace(value))
+                    .InvokeCommand(showHideCommand)
                     .DisposeWith(disposable);
             });
         }
